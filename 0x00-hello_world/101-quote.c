ecd38915003ed00cd28e1d059ccbf4c3bf5d4caf
Clairecynthia <clairetochi1@gmail.com>
2023-06-15 14:05:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <unistd.h>
 
@@ -11,7 +10,5 @@ int main(void)
 {
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-		
-
 	return (1);
 }
