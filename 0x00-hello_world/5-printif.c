ed952ed843c5e161189fc75c9c785266282c68eb
Clairecynthia <clairetochi1@gmail.com>
2023-06-15 12:18:53 -0700

    printif.c

---
+++ 0x00-hello_world/5-printif.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printif.c
+ * Auth: Emenike Claire
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammer, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
