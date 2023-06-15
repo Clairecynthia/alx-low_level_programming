03dd558c7b1a1e1b230ddf6af7809dd447112e8e
Clairecynthia <clairetochi1@gmail.com>
2023-06-15 13:43:51 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+/*
+ * File: 101-quote.c
+ * Auth: Emenike Claire
+ */
+
+#include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
+
+	return (1);
+}
