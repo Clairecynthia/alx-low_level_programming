16c81c6e19ac4055535bfecf1a7be72a1fa42b4d
Clairecynthia <clairetochi1@gmail.com>
2023-06-15 13:58:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,17 @@
-/*
- * File: 101-quote.c
- * Auth: Emenike Claire
- */
 
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - Entry prints 
  *
- * Return: Always 1.
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+		
 
 	return (1);
 }
