8045a92e64559a0819b6e7f190cbb92e870012c1
Clairecynthia <clairetochi1@gmail.com>
2023-06-16 03:23:27 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
-/*
- * File: 4-puts.c
- * Auth: Emenike Claire
- */
-
-#include <studio.h>
-
+#include <stdio.h>
 /**
- * main - prints "programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
