479fb493cc032366d48d6aa8b1a0e37b802153ac
Clairecynthia <clairetochi1@gmail.com>
2023-06-18 06:58:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,14 @@
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a  new line, to standard error.
+ *                followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-	      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
