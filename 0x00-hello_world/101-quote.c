a6e53db67c0fbc18cbd28af2685387e16cba00f6
Clairecynthia <clairetochi1@gmail.com>
2023-06-18 06:26:59 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,7 +14,7 @@
 int main(void)
 {
 	write(2,
-	      "and that piece of art is useful\' - Dora korpar, 2015-10-19\n", 59);
+	      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
