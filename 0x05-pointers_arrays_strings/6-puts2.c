96aeb2707a9698285c4445cce4784b07b5e6673d
Clairecynthia <clairetochi1@gmail.com>
2023-06-26 09:52:27 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 prints one char out of 2 of a string
+ * puts2 - prints one char out of 2 of a string
  * followed by a new line
  * @str: string to print the chars from
  */
