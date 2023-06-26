5c44313c68f66ada74016240b23f8018715065f3
Clairecynthia <clairetochi1@gmail.com>
2023-06-26 08:40:23 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I ddo not fear computers. I fear the lack of them - Isaac Asimov";
+	_putchar(str);
+	return (0);
+}
