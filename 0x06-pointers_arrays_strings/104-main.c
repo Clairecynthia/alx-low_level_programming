5cab1fa2461f0ceb249d7d6481fd6106b886a14b
Clairecynthia <clairetochi1@gmail.com>
2023-06-30 16:22:49 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/104-main.c
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -9,7 +9,8 @@
 int main(void)
 {
 	char buffer[] = "This is a string!\0And this the rest ofthe #buffer :)\1\2\3\4\5\6\7#cisfu
-		n\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
+		        n\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun;f
+			#infernumisfun\n";
 
 	printf("%s\n", buffer);
 	printf("---------------------------------\n");
