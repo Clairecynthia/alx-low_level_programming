0772bb397a3c82a5e86f17928066da14ff05742e
Clairecynthia <clairetochi1@gmail.com>
2023-07-11 14:15:40 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,25 +29,26 @@ char *argstostr(int ac, char **av)
 			size++;
 			i++;
 		}
-		arg = malloc((sizeof(char) * size) + 1);
-		if (arg == NULL)
-			return (NULL);
-		i = 0;
-		while (i < ac)
-		{
-			j = 0;
-			while (av[i][j])
-			{
-				arg[k] = av[i][j];
-				j++;
-				k++;
-			}
-			arg[k] = '\n';
-			k++;
-			i++;
-		}
-		arg[k] = '\0';
+		size++;
+		i++;
 	}
-
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			arg[k] = av[i][j];
+			j++;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+		i++;
+	}
+	arg[k] = '\0';
 	return (arg);
 }
