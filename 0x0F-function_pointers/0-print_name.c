6fb61f1d8a5247936a0e8b4ca7d64f05d38f09b1
Clairecynthia <clairetochi1@gmail.com>
2023-07-19 13:13:42 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,8 +8,8 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-        if (name == NULL || f == NULL)
-                return;
+	if (name == NULL || f == NULL)
+		return;
 
-        f(name);
+	f(name);
 }
