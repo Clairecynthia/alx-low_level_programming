7e7c566174df6bb76e7c79c36ba28b5d26982f4b
clairecynthia <clairetochi1@gmail.com>
2023-08-07 07:34:01 -0700

    -read_textfile.c

--- 0x06-pointers_arrays_strings/104-main.c
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-	char buffer[] = "This is a string!\0And this the rest ofthe #buffer :)\1\2\3\4\5\6\7#cisfu
-		        n\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun;f
-			#infernumisfun\n";
+    char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
 
-	printf("%s\n", buffer);
-	printf("---------------------------------\n");
-	print_buffer(buffer, sizeof(buffer));
-	return (0);
+    printf("%s\n", buffer);
+    printf("---------------------------------\n");
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
 }
+
